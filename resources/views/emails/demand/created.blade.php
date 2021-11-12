de5172f9b32471728d961a3231a335dd4baff551
Khwarz <karoumem01@gmail.com>
2021-11-12 10:43:09 +0100

    fixing stuffs inside the project

--- resources/views/emails/demand/created.blade.php
+++ resources/views/emails/demand/created.blade.php
@@ -1,5 +1,9 @@
-@component('mail::message') # Confirmation de la création de votre demande Votre
-demande a été créée avec succès. Voici le code de cette dernière: "{{
-$demand->payment_token }}". Il vous permettra de suivre l'avancement du
-traitement de votre demande. Thanks,<br />
-{{ config('app.name') }} @endcomponent
+@component('mail::message')
+# Confirmation de la création de votre demande
+
+Votre demande a été créée avec succès.
+Voici le code de cette dernière: "{{ $demand->payment_token }}".
+Il vous permettra de suivre l'avancement du traitement de votre demande.
+
+Merci,<br /> {{ config('app.name') }}
+@endcomponent
